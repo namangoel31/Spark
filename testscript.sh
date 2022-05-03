e5852f214ce9e65e627ee55705675a127fcd2de1
Naman Goel <43837926+namangoel31@users.noreply.github.com>
2022-05-04 03:57:35 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 hostname -I > Master.txt
+aws s3 cp /Master.txt s3://sparkonecs/Slave/Master.txt
 start-master.sh
+aws s3 cp s3://sparkonecs/Master/sleeptimer.py /home/Spark/sleeptimer.py
 python /home/Spark/sleeptimer.py
-aws s3 cp /Master.txt $BUCKET_URL/Master.txt
